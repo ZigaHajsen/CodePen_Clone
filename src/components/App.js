66ee4bc6a65fe304551964eb163f808d489c7669
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-09-22 22:19:20 +0200

    HTML setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-function App() {
-  return 'Hello World';
-}
+import Editor from './Editor';
+
+const App = () => {
+  return (
+    <Fragment>
+      <div className='pane top-pane'>
+        <Editor />
+        <Editor />
+        <Editor />
+      </div>
+      <div className='pane'>
+        <iframe
+          title='output'
+          sandbox='allow-scripts'
+          frameBorder='0'
+          height='100%'
+          width='100%'
+        />
+      </div>
+    </Fragment>
+  );
+};
 
 export default App;
