f4b60b9b9180845f205f0790de287c295a34d247
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-09-22 22:37:46 +0200

    Editor setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import Editor from './Editor';
 
 const App = () => {
+  const [html, setHtml] = useState('');
+  const [css, setCss] = useState('');
+  const [js, setJs] = useState('');
+
   return (
     <Fragment>
       <div className='pane top-pane'>
-        <Editor />
-        <Editor />
-        <Editor />
+        <Editor
+          language='xml'
+          displayName='HTML'
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language='css'
+          displayName='CSS'
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language='javascript'
+          displayName='JS'
+          value={js}
+          onChange={setJs}
+        />
       </div>
       <div className='pane'>
         <iframe
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Controlled as ControlledEditor } from 'react-codemirror2';
 
-const Editor = () => {
-  return <div>Editor</div>;
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/css/css';
+
+const Editor = (props) => {
+  const { language, displayName, value, onChange } = props;
+
+  function handleChange(editor, data, value) {
+    onChange(value);
+  }
+
+  return (
+    <div className='editor-container'>
+      <div className='editor-title'>
+        {displayName}
+        <button>O/C</button>
+      </div>
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className='code-mirror-wrapper'
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          lineNumbers: true,
+          theme: 'material',
+        }}
+      />
+    </div>
+  );
 };
 
 export default Editor;
